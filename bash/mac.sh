45114fb098c650e0c0d824c435eff4baac1f5846
HowesPhoto <Paul@HowesPhoto.com>
2013-06-12 12:17:36 -0400

    Mac-specific bash script.

---
+++ bash/mac.sh
@@ -0,0 +1,3 @@
+# A command that fixes the "Open With..." context menu in Finder.
+alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
+
