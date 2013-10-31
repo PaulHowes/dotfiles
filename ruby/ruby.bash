f1c07b263b1b19841c12952d23b2e465a4d75ac0
HowesPhoto <Paul@HowesPhoto.com>
2013-10-31 13:55:24 -0400

    Updated Ruby script to check for path.
    The bin path was incorrectly assumed to exist, which is not true
    if Ruby has not been installed.

--- ruby/ruby.bash
+++ ruby/ruby.bash
@@ -1,2 +1,7 @@
 # Adds the path Homebrew expects gems to be installed into.
-export PATH=/usr/local/opt/ruby/bin:${PATH}
+RUBY_BIN=/usr/local/opt/ruby/bin
+if [ -e ${RUBY_BIN} ]
+then
+  export PATH=${RUBY_BIN}:${PATH}
+fi
+
